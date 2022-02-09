5d6a628562938a57835ac659e98a16813679e5e7
asagiman <lagavulin.jb.dc@gmail.com>
2022-02-09 21:20:14 +0900

    1

--- RemoveIt.py
+++ RemoveIt.py
@@ -2,12 +2,22 @@
 # A から X と等しい要素を全て取り除き、残った要素をそのままの順序で並べた数列 A′
 # を出力してください。
 
-N,X = map(int,input().split())
-A = list(map(int, input().split()))
-ans = []    
+# N,S,D = map(int, input().split())
 
-for i in range(N):
-    if A[i] != X:
-        ans.append(A[i])
+# for i in range(N):
+#     X, Y = map(int, input().split())
+#     if X < S and D < Y:
+#         print("Yes")
+#         exit()
 
-print(*ans)
+# print("No")
+
+
+N, S, D = map(int, input().split())
+for _ in range(N):
+  X, Y = map(int, input().split())
+  if X < S and Y > D:
+    print('Yes')
+    break
+else:
+  print('No')
