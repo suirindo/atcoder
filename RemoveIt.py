13cc72209298f9e9f401464cd2b97b8ac7c02ba9
asagiman <lagavulin.jb.dc@gmail.com>
2022-02-09 20:48:35 +0900

    1

---
+++ RemoveIt.py
@@ -0,0 +1,13 @@
+# 長さ N の整数列 A と整数 X が与えられます。
+# A から X と等しい要素を全て取り除き、残った要素をそのままの順序で並べた数列 A′
+# を出力してください。
+
+N,X = map(int,input().split())
+A = list(map(int, input().split()))
+ans = []    
+
+for i in range(N):
+    if A[i] != X:
+        ans.append(A[i])
+
+print(*ans)
