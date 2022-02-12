a309570f161814fc4b290beff0fa9e5c24212ffd
asagiman <lagavulin.jb.dc@gmail.com>
2022-02-12 13:08:25 +0900

    1

---
+++ cpp/helloworld.cpp
@@ -0,0 +1,20 @@
+/* #include <bits/stdc++.h>はC++の機能をすべて読み込むための命令
+ cout endl だけであれば #include <iostream> で足りる
+
+using namespace std; プログラムを短く書くための機能
+#includeで読み込んだC++の機能を利用するためには、通常はstd::coutやstd::endlのようにstd::をはじめに付ける必要があります。
+using namespace std;を利用すると、このstd::を省略して書くことができます。*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cout << "the world!" ;
+    cout << "a";
+    cout << "b" << endl;
+    cout << "c" << "d" << endl;
+    cout << "こんにちは" << endl;
+    cout << "AtCoder" << endl;
+
+}
