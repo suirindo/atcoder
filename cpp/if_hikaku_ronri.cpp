71b6b80191bc88f8137bcd1f202fc32733cee599
asagiman <lagavulin.jb.dc@gmail.com>
2022-02-12 15:17:05 +0900

    1

--- cpp/if_hikaku_ronri.cpp
+++ cpp/if_hikaku_ronri.cpp
@@ -2,15 +2,38 @@
 using namespace std;
 
 int main() {
-    int x;
-    cin >> x;
-    if(x < 10) {
-        cout << "xは10より小さい" << endl;
+    // int x;
+    // cin >> x;
+    // if(x < 10) {
+    //     cout << "xは10より小さい" << endl;
+    // }
+    // else if ((10 < x) && (x < 20) ) {
+    //     cout << "xは20より小さい" << endl;
+    // }
+    // else {
+    //     cout << "xは20より大きい" << endl;
+    // }
+    
+        int A, B;
+        string op;
+        cin >> A >> op >> B;
+
+        if ( op == "+") {
+            cout << A + B << endl;
+        }
+        else if ( op == "-") {
+            cout << A - B << endl;
+        }
+        else if ( op == "*") {
+            cout << A * B << endl;
+        }
+        else if ( op == "/" && B != 0) {
+            cout << A / B << endl;
+            // if ( B == 0){
+            //     cout << "error" << endl;
+            // }
+        }
+        else {
+            cout << "error" << endl;
+        }
     }
-    else if ((10 < x) && (x < 20) ) {
-        cout << "xは20より小さい" << endl;
-    }
-    else {
-        cout << "xは20より大きい" << endl;
-    }
-}
