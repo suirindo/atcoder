26540c24febed11e8a65afbc85804edd189ff5f2
asagiman <lagavulin.jb.dc@gmail.com>
2022-02-15 23:19:07 +0900

    1

--- cpp/多重ループ.cpp
+++ cpp/多重ループ.cpp
@@ -1,6 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int main(){
+    int N ,S;
+    cin >> N >> S;
+    vector<int> A(N), P(N);
+    for (int i = 0; i < N; i++){
+        cin >> A.at(i);
+    }
+    for (int i = 0; i < N; i++){
+        cin >> P.at(i);
+    }
+    //りんご・パイナップルをそれぞれ１つずつ購入するとき合計S円になるような買い方が何通りあるか
+    int count = 0;
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++){
+            if(A.at(i) + P.at(j) == S){
+                count++;
+            }
+        }
+    }
+    cout << count << endl;
+}
+
+
+
+
+/*
 //多重ループの関数化
 void func() {
     int sum = 0;
@@ -20,6 +46,7 @@ void func() {
 int main(){
     func();
 }
+*/
 
 
 
