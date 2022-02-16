bb0e038cb6ae27dabac04174345f6451bad3f391
asagiman <lagavulin.jb.dc@gmail.com>
2022-02-16 23:51:15 +0900

    1

---
+++ cpp/多次元配列.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    // N * ( 3 * 3)要素の配列を宣言
+    vector<vector<vector<char>>> data(N, vector<vector<char>>(3, vector<char>(3)));
+
+    //入力
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < 3; j++){
+            for (int k = 0; k < 3; k++){
+                cin >> data.at(i).at(j).at(k);
+            }
+        }
+    }
+
+    // '●'の数を数える
+    int count = 0;
+
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < 3; j++){
+            for (int k = 0; k < 3; k++){
+                if ( data.at(i).at(j).at(k) == 'o'){
+                    count++;
+                }
+            }
+        }
+    }
+    cout << count << endl;
+}
+
+
+
+
+/*
+int main(){
+    //int型の２次元配列（3*4)の宣言
+    vector<vector<int>> data(3, vector<int>(4));
+
+    //入力（２重ループ）
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 4; j++){
+            //２次元配列の要素へのアクセス方法。上から何番目、左から何番目
+            cin >> data.at(i).at(j);
+        }
+    }
+
+    //0の個数を数える
+    int count = 0;
+
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 4; j++){
+            if( data.at(i).at(j) == 0){
+                count++;
+            }
+        }
+    }
+    cout << count << endl;
+}
+*/
