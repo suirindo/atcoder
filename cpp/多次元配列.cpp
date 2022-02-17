1034250b832e19d88b456eb0dbc5d0fd7a0b4ab0
asagiman <lagavulin.jb.dc@gmail.com>
2022-02-17 22:03:46 +0900

    1

--- cpp/多次元配列.cpp
+++ cpp/多次元配列.cpp
@@ -2,6 +2,38 @@
 using namespace std;
 
 int main(){
+    int N, M;
+    cin >> N >> M ;
+    vector<int> A(M), B(M);
+    for (int i = 0; i < M; i++){
+        cin >> A.at(i) >> B.at(i);
+    }
+
+    // N * Nのchar型の２次元配列のすべての要素を'-'で初期化
+    vector<vector<char>> table(N, vector<char>(N, '-'));
+
+    for (int i = 0; i < M; i++) {
+        // 1~N → 0~N-1に変換
+        A.at(i)--; B.at(i)--;
+        table.at(A.at(i)).at(B.at(i)) = 'o';
+        table.at(B.at(i)).at(A.at(i)) = 'x';
+    }
+
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+            cout << table.at(i).at(j);
+            if(j == N-1){
+                cout << endl;
+            }
+            else {
+                cout << ""; //行末でないなら空白を出力
+            }
+        }
+    }
+}
+
+/*
+int main(){
     int N;
     cin >> N;
     // N * ( 3 * 3)要素の配列を宣言
@@ -33,8 +65,6 @@ int main(){
 
 
 
-
-/*
 int main(){
     //int型の２次元配列（3*4)の宣言
     vector<vector<int>> data(3, vector<int>(4));
