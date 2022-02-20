255c7fbd4ce1d0cf7de864bab1ba02dfd3a125f3
asagiman <lagavulin.jb.dc@gmail.com>
2022-02-20 15:37:56 +0900

    1

--- cpp/ShiftOnly.cpp
+++ cpp/ShiftOnly.cpp
@@ -1,20 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int N;
+int A[210];
+
 int main(){
-    int N;
-    vector<int> data(N);
     cin >> N;
-    int counter = 0;
-    for (int i = 0; i < N; i++){
-        cin >> data.at(i);
-    }
-    for (int i = 0; i < N; i++){
-        if (data.at(i) % 2 == 0){
-            counter++;
-        }else{
-            break;
+    for(int i = 0; i < N; i++) cin >> A[i];
+
+    int res = 0;
+
+    //操作が行える限り操作を繰り返す
+    while(true){
+        bool exist_odd = false; // A[i]がすべて偶数かどうか判定するフラグ
+        for(int i = 0; i < N; i++){
+            if(A[i] % 2 !=0) exist_odd = true; //奇数があったらフラグを立てる
         }
+
+        if(exist_odd) break; //奇数があったらbreak
+
+        //操作を行えるなら操作を実際に行う
+        for(int i = 0; i < N; i++){
+            A[i] /= 2;
+        }
+        ++res; //操作回数をインクリメント
     }
-    cout << counter << endl;
+    cout << res << endl;
 }
