56f1105c57f98176e1f3d3feca7c27ce85418dad
asagiman <lagavulin.jb.dc@gmail.com>
2022-02-27 10:26:01 +0900

    1

---
+++ cpp/DigitMachine.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+signed main(){
+    vector<ll>a(10);
+    for (int i = 0; i <= 9; i++){
+        cin >> a[i];
+    }
+
+    ll num = 0;
+
+    num = a[num];
+    num = a[num];
+    num = a[num];
+
+    cout << num << endl;
+
+    return 0;
+}
--- cpp/Pasta.cpp
+++ cpp/Pasta.cpp
@@ -1,10 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//解法2
 int main(){
-    int N, M;
-    vector<int> A(x);
-    vector<int> B(y);
+    int n , m;
+    map<int, int>mp;
+    int x;
 
-    
+    cin >> n >> m;
+    for (int i = 0; i < n; i++){
+        cin >> x;
+        mp[x]++;
+    }
+
+    for (int i = 0; i < m; i++){
+        cin >> x;
+        if (mp[x] == 0){
+            cout << "No" << endl;
+            return 0;
+        }
+        mp[x]--;
+    }
+    cout << "Yes" << endl;
+    return 0;
 }
+
+
+/*
+// 解法1
+int main(){
+    int n, m;
+    int a[1000];
+    int b;
+    bool used[1000];
+    bool found, success;
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++){
+        cin >> a[i];
+        used[i] = false;
+    }
+
+    success = true;
+    for (int i = 0; i < m; i++){
+        cin >> b;
+        found = false;
+        for(int j = 0; j < n; j++){
+            if((a[j] == b) && (!used[j])) {
+                used[j]  = true;
+                found = true;
+                break;
+            }
+        }
+        if(!found)success = false;
+    }
+    if(success) cout << "Yes" << false;
+    else cout << "No" << endl;
+    return 0;
+}
+
+*/
