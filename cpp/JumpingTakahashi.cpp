967e742f3c00d85df98d277578c201669c53bf83
asagiman <lagavulin.jb.dc@gmail.com>
2022-03-04 21:19:59 +0900

    1

--- cpp/JumpingTakahashi.cpp
+++ cpp/JumpingTakahashi.cpp
@@ -4,8 +4,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-using P = pair<int, int>;
+//using P = pair<int, int>;
 
+
+
+int main() {
+  int n, X;
+  cin >> n >> X;
+  vector<int> a, b;
+  a = vector<int>(n);
+  b = vector<int>(n);
+  rep(i,n) cin >> a[i] >> b[i];
+
+  vector dp(n+1, vector<bool>(X+101, false));
+  dp[0][0] = true;
+  rep(i,n) {
+    rep(x,X+1) {
+      if (dp[i][x]) {
+        dp[i+1][x+a[i]] = true;
+        dp[i+1][x+b[i]] = true;
+      }
+    }
+  }
+
+  if (dp[n][X]) cout << "Yes" << endl;
+  else cout << "No" << endl;
+  return 0;
+}
+
+
+
+
+
+
+/*
 int n, X;
 vector<int> a, b;
 map<P, bool> memo;
@@ -32,7 +64,7 @@ int main(){
 
 
 
-/*
+
 int main(){
     int n, x;
     cin >> n >> x;
