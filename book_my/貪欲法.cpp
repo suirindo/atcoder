054f03ecef1978cf56dbc09412bcc0c71b96cbf7
asagiman <lagavulin.jb.dc@gmail.com>
2022-03-19 12:00:18 +0900

    1

--- book_my/貪欲法.cpp
+++ book_my/貪欲法.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 typedef long long ll;
 
+/**
+例題：
 int main(){
     ll N, ans = 0;
     cin >> N;
@@ -12,5 +14,22 @@ int main(){
     while (N >= 1) { N -= 1000; ans += 1;}
     cout << ans <<endl;
     return 0;
+}
+**/
 
+ll N;
+ll A[100009], B[100009];
+
+int main(){
+    cin >> N;
+    for (int i = 1; i <= N; i++) cin >> A[i];
+    for (int i = 1; i <= N; i++) cin >> B[i];
+
+    sort(A + 1, A + N + 1);
+    sort(B + 1, B + N + 1);
+
+    ll ans = 0;
+    for (int i = 1; i <= N; i++) ans += abs(A[i] - B[i]);
+    cout << ans << endl;
+    return 0;
 }
